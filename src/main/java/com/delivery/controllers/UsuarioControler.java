731ec8a00762d6bc1b2472129761f4bd922b083a
GustavoRodrigue <gustavorodriguesgra13@gmail.com>
2022-09-22 20:27:16 -0300

    criando end-point listar todos em usuario controller

--- src/main/java/com/delivery/controllers/UsuarioControler.java
+++ src/main/java/com/delivery/controllers/UsuarioControler.java
@@ -1,8 +1,11 @@
 package com.delivery.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,4 +29,16 @@ public class UsuarioControler {
 		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
 		
 	}
+	
+	@GetMapping(value = "listatodos")
+	@ResponseBody
+	public ResponseEntity<List<Usuario>> listaUsuario(){
+		
+		List<Usuario> usuarios = repository.findAll();
+		
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+		
+	}
+	
+	
 }
