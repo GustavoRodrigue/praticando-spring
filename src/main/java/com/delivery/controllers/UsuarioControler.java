ec7e0cf7ee3b1d6e7e269d1226717fe172e9468e
GustavoRodrigue <gustavorodriguesgra13@gmail.com>
2022-09-22 20:08:44 -0300

    criando end-point salvar em usuario controller

--- src/main/java/com/delivery/controllers/UsuarioControler.java
+++ src/main/java/com/delivery/controllers/UsuarioControler.java
@@ -1,5 +1,29 @@
 package com.delivery.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.delivery.model.Usuario;
+import com.delivery.repository.UsuarioRepository;
+
+@RestController
 public class UsuarioControler {
 
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@PostMapping(value = "salvar")
+	@ResponseBody
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){
+		
+		Usuario user = repository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+		
+	}
 }
